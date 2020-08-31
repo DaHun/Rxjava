c68d7a6547674e9af11453d9819c62bf495ab6aa
unknown <dh9694@hanmail.net>
2020-08-31 22:26:29 +0900

    concatMap Function 직접 인자로 넣음

--- ConcatMapTest2.java
+++ ConcatMapTest2.java
@@ -2,7 +2,10 @@ package RxjavaPractice;
 
 import java.util.concurrent.TimeUnit;
 
+import org.reactivestreams.Publisher;
+
 import io.reactivex.Flowable;
+import io.reactivex.functions.Function;
 
 public class ConcatMapTest2 {
 	public static void main(String[] args) throws Exception {
@@ -19,7 +22,27 @@ public class ConcatMapTest2 {
 												
 		flowable.subscribe(new DebugSubscriber<>());
 		
-		Thread.sleep(4000L);
+		
+		
+		Function function = new Function<Integer, Publisher<? extends String>>(){
+
+			@Override
+			public Publisher<? extends String> apply(Integer sourceData) throws Exception {
+				// TODO Auto-generated method stub
+				return Flowable.interval(400L, TimeUnit.MILLISECONDS)
+								.map(data -> "["+sourceData+"] "+data);
+			}
+			
+		};
+		
+	
+		flowable = Flowable.range(10,3)
+				.concatMap(function);
+		
+		
+		flowable.subscribe(new DebugSubscriber<>());
+		
+		Thread.sleep(7000L);
 		
 	}
 	
